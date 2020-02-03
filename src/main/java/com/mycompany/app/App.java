747c2bf2c747c974e95a191a3dc21d27b38d08d7
npmahapatra <60578508+npmahapatra@users.noreply.github.com>
2020-02-03 17:59:35 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!";
+    private final String message = "A Simple Java Application - My first step to intuit !";
 
     public App() {}
 
